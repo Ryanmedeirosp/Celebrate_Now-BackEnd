121cf6e29d7e806965dd94382880a464287b8132
Júnior <josebgojunior@gmail.com>
2025-01-31 19:23:11 -0300

    feat(junior)

--- backend/src/main/java/com/celebrate/backend/models/Dto/GetSupplier.java
+++ backend/src/main/java/com/celebrate/backend/models/Dto/GetSupplier.java
@@ -8,12 +8,16 @@ public class GetSupplier {
     private String email;
     private String phone;
     private String cnpj;
-
-    public GetSupplier(String name, String email, String phone, String cnpj) {
+        private String serviceType;
+                private String description;
+            
+    public GetSupplier(String name, String email, String phone, String cnpj, String serviceType, String description) {
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.cnpj = cnpj;
+        this.serviceType = serviceType;
+        this.description = description;
     }
 
 }
