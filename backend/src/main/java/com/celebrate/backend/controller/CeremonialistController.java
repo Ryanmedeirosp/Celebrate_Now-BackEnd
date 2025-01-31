a5e3135e34f0a0c2751d89870f769b6a16340161
ryan medeiros <ryan.mporciuncula@gmail.com>
2025-01-31 19:24:34 -0300

    adding strret on supplier - feat (ryan)

--- backend/src/main/java/com/celebrate/backend/controller/CeremonialistController.java
+++ backend/src/main/java/com/celebrate/backend/controller/CeremonialistController.java
@@ -1,6 +1,5 @@
 package com.celebrate.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
--- backend/src/main/java/com/celebrate/backend/models/Dto/GetSupplier.java
+++ backend/src/main/java/com/celebrate/backend/models/Dto/GetSupplier.java
@@ -8,11 +8,13 @@ public class GetSupplier {
     private String email;
     private String phone;
     private String cnpj;
+    private String street;
 
-    public GetSupplier(String name, String email, String phone, String cnpj) {
+    public GetSupplier(String name, String email, String phone, String cnpj, String street) {
         this.name = name;
         this.email = email;
         this.phone = phone;
+        this.street = street;
         this.cnpj = cnpj;
     }
 
